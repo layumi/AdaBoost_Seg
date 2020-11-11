17460332b696f75425815f9043bca095785539a2
Hu Zhang <hzhang6@venus19.eng.uts.edu.au>
2020-11-11 21:01:08 +1100

    update evaluate folder

--- evaluate_cityscapes_folder.py
+++ evaluate_cityscapes_folder.py
@@ -3,9 +3,14 @@ import argparse
 import os
 parser = argparse.ArgumentParser(description='Test')
 parser.add_argument('--name', default='SWA_MRNet', type=str, help='save model path')
+parser.add_argument('--average', action='store_true', help='using average model')
 opt = parser.parse_args()
 result = './snapshots/%s/result.txt'%opt.name
 
 for iter in np.arange(1,11):
-    os.system('echo %d | tee -a %s'%(iter*10000, result))
-    os.system('python evaluate_cityscapes.py --restore ./snapshots/%s/GTA5_%d_average.pth --batchsize 12 | tee -a %s'%(opt.name,iter*10000, result))
+    if opt.average:
+        os.system('echo %d+%s | tee -a %s'%(iter*10000, 'average', result))
+        os.system('python evaluate_cityscapes.py --restore ./snapshots/%s/GTA5_%d_average.pth --batchsize 12 | tee -a %s'%(opt.name,iter*10000, result))
+    else:
+        os.system('echo %d | tee -a %s'%(iter*10000, result))
+        os.system('python evaluate_cityscapes.py --restore ./snapshots/%s/GTA5_%d.pth --batchsize 12 | tee -a %s'%(opt.name,iter*10000, result))
