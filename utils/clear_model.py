4900087c7b756b3e80bab6d35a6d319bcb589f7b
layumi <zdzheng12@gmail.com>
2021-02-09 00:36:23 +1100

    rm dis

--- utils/clear_model.py
+++ utils/clear_model.py
@@ -8,6 +8,11 @@ for f in os.listdir(root):
         for fff in os.listdir(dir_name):
             if fff =='opts.yaml':
                 continue
+            if fff.endswith('D1.pth') or fff.endswith('D2.pth'):
+                    dst = dir_name+'/'+fff
+                    print(dst)
+                    os.remove(dst)
+
             try:
                 if int(fff[5:10])<25000 or int(fff[5:10])==30000 or int(fff[5:10])==35000 or int(fff[5:10])==40000 or int(fff[5:10])==45000 or int(fff[5:10])>70000:
                     dst = dir_name+'/'+fff
@@ -15,3 +20,4 @@ for f in os.listdir(root):
                     os.remove(dst)
             except:
                 continue
+
