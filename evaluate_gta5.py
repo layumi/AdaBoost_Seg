c7da2faa84ed40c0598ea835fe591cf4c691742f
Zhedong Zheng <Zhedong.Zheng@student.uts.edu.au>
2021-03-17 11:14:10 +0800

    Update evaluate_gta5.py

--- evaluate_gta5.py
+++ evaluate_gta5.py
@@ -20,6 +20,7 @@ from PIL import Image
 from utils.tool import fliplr
 import matplotlib.pyplot as plt
 import torch.nn as nn
+import yaml
 IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
 
 # We just use this file to evaluate the perfromance on the training set
