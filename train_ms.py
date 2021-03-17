aadff239a6c6c3449324f4f181ba1254159ad269
Zhedong Zheng <Zhedong.Zheng@student.uts.edu.au>
2021-03-17 14:33:16 +0800

    Update train_ms.py

--- train_ms.py
+++ train_ms.py
@@ -264,7 +264,7 @@ def main():
     targetloader2 = data.DataLoader(cityscapesDataSet(args.data_dir_target, args.data_list_target, crop_size=(448, 896), resize_size=(1024, 512), mean=IMG_MEAN, scale=False, mirror=False, set='train'),
                            batch_size=8, shuffle=False, pin_memory=True, num_workers=4)
     targetloader2_shuffle = data.DataLoader(cityscapesDataSet(args.data_dir_target, args.data_list_target, crop_size=(448, 896), resize_size=(1024, 512), mean=IMG_MEAN, scale=False, mirror=True, set='train'),
-                           batch_size=24, shuffle=True, pin_memory=True, num_workers=4)
+                           batch_size=18, shuffle=True, pin_memory=True, num_workers=4)
 
     # set up tensor board
     if args.tensorboard:
