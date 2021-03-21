de81ccca3a5fa7cec4d43ca4d018f61e63ec4bff
Zhedong Zheng <Zhedong.Zheng@student.uts.edu.au>
2021-03-21 22:12:02 +0800

    Update evaluate_cityscapes.py

--- evaluate_cityscapes.py
+++ evaluate_cityscapes.py
@@ -205,6 +205,8 @@ def main():
         image, _, _, name = batch
         image2, _, _, name2 = batch2
         #image3, _, _, name3 = batch3
+        if args.model == 'DeepVGG': # pytorch vgg16 accepts normalized inputs.
+            image, image2 = image/255.0, image2/255.0 
 
         inputs = image.cuda()
         inputs2 = image2.cuda()
