7c1ed8f0fca868d225ab91395ea4e87bbd80d265
layumi <zdzheng12@gmail.com>
2021-09-06 22:58:10 +1000

    update remove

--- utils/clear_model.py
+++ utils/clear_model.py
@@ -6,7 +6,7 @@ for f in os.listdir(root):
     for ff in os.listdir(root+f):
         dir_name = root+f
         for fff in os.listdir(dir_name):
-            if fff =='opts.yaml':
+            if fff =='opts.yaml' or fff=='GTA5_100000.pth':
                 continue
             if fff.endswith('D1.pth') or fff.endswith('D2.pth'):
                     dst = dir_name+'/'+fff
@@ -14,10 +14,14 @@ for f in os.listdir(root):
                     os.remove(dst)
 
             try:
+                if int(fff[5:10])//10000==1 and not fff.endswith('average.pth'):
+                    dst = dir_name+'/'+fff
+                    print(dst)
+                    #os.remove(dst)
                 if not(int(fff[5:10])%10000==0) and not fff.endswith('average.pth'):
                     dst = dir_name+'/'+fff
                     print(dst)
-                    os.remove(dst)
+                    #os.remove(dst)
             except:
                 continue
 
